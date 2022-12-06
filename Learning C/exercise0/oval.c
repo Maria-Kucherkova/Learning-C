2b4b82a7ca4ac9093ef3d9a39f59f22dd4c30de1
Maria-Kucherkova <94479638+Maria-Kucherkova@users.noreply.github.com>
2022-12-07 00:48:15 +0300

    Add files via upload

---
+++ Learning C/exercise0/oval.c
@@ -0,0 +1,8 @@
+#include "graphics.h"
+
+int main(void)
+{
+	drawOval(50,100,200,150);
+	
+	return 0;
+}
---
+++ Learning C/exercise0/rectangle.c
@@ -0,0 +1,10 @@
+#include "graphics.h"
+
+int main(void)
+{
+	drawRect(50,100,200,150);
+	setColour(blue);
+	fillRect(50,100,200,150);
+	
+	return 0;
+}
