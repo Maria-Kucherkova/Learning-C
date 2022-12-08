661a614c8f66f897fb36a4caafb37a335781710c
Maria Kucherkova <mkucherkova@gmail.com>
2022-12-08 18:30:04 +0300

    added Q1

---
+++ exercise1/q1.c
@@ -0,0 +1,17 @@
+// Written by <Maria Kucherkova, 08/12/2022>
+// This program displays my name.
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char myName[] = "Maria Kucherkova";
+	printf("Hello, my name is %s\n", myName);
+	int index = strlen(myName) - 1;
+	while (index > -1)
+	{
+    printf("%c", myName[index]);
+    index = index - 1;
+	}
+	printf("\n");
+return 0; 
+}
