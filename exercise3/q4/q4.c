32f18d04a6ed4253edb01426e514e4102f1ca621
Maria Kucherkova <mkucherkova@gmail.com>
2023-08-18 16:25:31 +0100

    Q4 E3 pushed

--- exercise3/q4/q4.c
+++ exercise3/q4/q4.c
@@ -1,47 +1,49 @@
 #include <stdio.h>
 
-void readRange()
+void readRange(int *number1, int *number2)
 {
-
+	printf("Enter a range: ");
+    scanf("%d %d", number1, number2);
 }
 
-void sortNums()
+void sortNums(int *number1, int *number2)
 {
-
+	if (*number2 < *number1) {
+		int temp = *number1;
+		*number1 = *number2;
+		*number2 = temp;
+	}
 }
 
 int isOneNegative(int number1, int number2)
 {
-	if ((number1 < 0 && number2 > 0) || (number1 > 0 || number2 < 0))
+	if ((number1 < 0 && number2 > 0) || (number1 > 0 && number2 < 0))
 		return -1;
 	return 0;
-
 }
 
-int recursiveCalculation(int number1, int number2, int result)
-{
-	if (number2 < number1)
-	{
-		return result;
-	}
-	return recursiveCalculation(number1, ((number2)-1), result * number2);
+int loopCalculation(int number1, int number2, int result) {
+    while (number1 <= number2) {  
+        result = result * number2;
+        number2--;
+    }
+    return result;
 }
 
 int main() {
 	int number1, number2;
 
-	// Your work goes here
-	readRange();
-	sortNums();
-	// Your work goes here
+	readRange(&number1, &number2);
+	sortNums(&number1, &number2);
 
-	if (isOneNegative(number1, number2) == 0)
-	{
+	printf("number1: %d\n", number1);
+	printf("number2: %d\n", number2);
+
+	if (isOneNegative(number1, number2) == -1) {
 		printf("Result is zero");
 	}
-	else
-	{
-		printf("%d", recursiveCalculation(number1, number2, 1));
+	else {
+		printf("%d", loopCalculation(number1, number2, 1));
 	}
 	
     return 0;
